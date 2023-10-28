8290d0bf83f23df48b7d3e191f3102ceccefeb89
Josh <93979045+Skidesign1@users.noreply.github.com>
2023-10-28 16:50:11 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.externals.push({
-      sharp: "commonjs sharp",
-      canvas: "commonjs canvas",
-    });
-    return config;
-  },
-};
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
 
-module.exports = nextConfig;
+module.exports = {
+  reactStrictMode: true,
+};
