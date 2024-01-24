8403651155afcdef6e9b597089fc0f11534b1ff9
Modupe Akanni <modupe775@gmail.com>
2024-01-24 14:44:48 +0100

    Update text panel

--- src/components/panels/TextResourcesPanel.tsx
+++ src/components/panels/TextResourcesPanel.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { observer } from "mobx-react";
-import { TextResource } from "../entity/TextResource";
+import { TextResource } from "../entity/TextResource"; 
 
 export const TextResourcesPanel = observer(() => {
   return (
@@ -9,16 +9,16 @@ export const TextResourcesPanel = observer(() => {
       <div className="text-sm px-[16px] pt-[16px] pb-[8px] font-semibold text-white">
         Add Text
       </div>
-      <TextResource sampleText="Title" fontSize={28} fontWeight={600} />
-      <TextResource sampleText="Subtitle" fontSize={16} fontWeight={600} />
-      <TextResource sampleText="Body" fontSize={14} fontWeight={400} />
-      <TextResource sampleText="Caption" fontSize={12} fontWeight={400} />
-      <TextResource sampleText="Heading 1" fontSize={24} fontWeight={800} />
-      <TextResource sampleText="Heading 2" fontSize={20} fontWeight={800} />
-      <TextResource sampleText="Heading 3" fontSize={18} fontWeight={800} />
+      <TextResource sampleText="Headline" fontSize={28} fontWeight={600} />
+      <TextResource sampleText="Body Text" fontSize={16} fontWeight={600} />
+      <TextResource sampleText="Call to Action" fontSize={14} fontWeight={400} />
+      <TextResource sampleText="Phone Number" fontSize={12} fontWeight={400} />
+      <TextResource sampleText="Email" fontSize={24} fontWeight={800} />
+      <TextResource sampleText="Website" fontSize={20} fontWeight={800} />
+      {/* <TextResource sampleText="Heading 3" fontSize={18} fontWeight={800} />
       <TextResource sampleText="Heading 4" fontSize={16} fontWeight={800} />
       <TextResource sampleText="Heading 5" fontSize={14} fontWeight={800} />
-      <TextResource sampleText="Heading 6" fontSize={12} fontWeight={800} />
+      <TextResource sampleText="Heading 6" fontSize={12} fontWeight={800} /> */}
     </div>
   );
 });
