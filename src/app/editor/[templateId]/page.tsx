097de75ebbd30033c3fe7f527f11239b77fd719f
Modupe Akanni <modupe775@gmail.com>
2024-02-18 22:40:40 +0100

    update template

--- src/app/editor/[templateId]/page.tsx
+++ src/app/editor/[templateId]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { StoreProvider } from "@/store";
@@ -9,7 +10,13 @@ import { Editor } from "../../../components/Editor";
 function EditorPage( ) {
 
     const params = useParams<{ templateId: string }>()
+    const searchParams = useSearchParams()
+    const userId = searchParams.get('userId')
+   if (userId) {
+    window.sessionStorage.setItem('userId', userId);
+  }
     const [templateInfo, setTemplateInfo] = useState(null);
+    const [userInfo, setUserInfo] = useState(null);
   
     useEffect(() => {
       const fetchTemplateInfo = async () => {
@@ -22,13 +29,30 @@ function EditorPage( ) {
           console.error('Error fetching template info:', error);
         }
       };
+
+      const userId = window.sessionStorage.getItem('userId');
+      const fetchUserInfo = async () => {
+        try {
+          // Make a POST request to fetch user info using the provided id
+          const response = await axios.get(`https://skyestudio-backend.onrender.com/user/profile`);
+          console.log(response)
+          setUserInfo(response.data);
+        } catch (error) {
+          console.error('Error fetching user info:', error);
+        }
+      };
   
       if (params.templateId) {
         fetchTemplateInfo();
       }
-    }, [params.templateId]);
+
+      if (userId) {
+        fetchUserInfo();
+      }
+    }, [params.templateId, userId]);
 
   return (
+    userId &&
     <StoreProvider>
       <p className=" ml-5"><img src="/favicon.png" className="h-[40px] w-[40px] ml-5 mt-5" /> Skye Studio</p>
       <Editor/>
